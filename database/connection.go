51b8c29245b20eef714616fee7cf3e2ac3873bae
Lee ShaoWee <lee_shao_wee@hotmail.com>
2022-12-25 14:07:23 +0800

    modify dummy data

--- database/connection.go
+++ database/connection.go
@@ -33,21 +33,21 @@ func CreateTables(db *gorm.DB) {
 }
 
 func AddDummyData() {
-	AddUser("abby@test.com", "12356", "abby", 1, "dark")
-	AddUser("bob@test.com", "s9614067c", "bob", 1, "red")
-	AddUser("cassie@test.com", "456789", "cassie", 2, "pink")
-	AddUser("shaogamers@gmail.com", "719346", "shao", 1, "grape")
-	AddUser("shao.lee@cldcvr.com", "719346", "leeshaowee", 1, "violet")
+	AddUser("abby@test.com", "abby123456", "abby", 1, "dark")
+	AddUser("bob@test.com", "bob123456", "bob", 1, "red")
+	AddUser("cassie@test.com", "cassie123456", "cassie", 2, "pink")
+	AddUser("david@test.com", "david123456", "david", 2, "blue")
+	AddUser("emily@test.com", "emily123456", "emily", 1, "violet")
 
-	AddPost(4, "title1", "this is some body 1", "food,groceries,design")
-	AddPost(1, "title2", "this is some body 2", "sports,groceries,design")
-	AddPost(2, "title3", "this is some body 3", "shopping,groceries,entertainment")
+	AddPost(1, "How to live healthy?", "Drink more water, get enough sleep, eat more fruits and vegetables and cut down on processed food", "food,lifestyle,health tips")
+	AddPost(2, "Sleep deprivation", "Sleep deprivation is when you don’t get the sleep you need, and it is It’s estimated to affect around one-third of American adults , a problem that has only worsened in recent years. Lack of sleep directly affects how we think and feel. While the short-term impacts are more noticeable, chronic sleep deprivation can heighten the long-term risk of physical and mental health problems.", "lifestyle,sleep,health tips")
+	AddPost(3, "Worst Foods to Eat for Your Health", "Foods with added sugar or salt or refined carbohydrates, processed meats", "food,groceries")
 
-	AddComment(2, 1, "this is some content 1")
-	AddComment(1, 1, "this is some content 2")
-	AddComment(2, 2, "this is some content 3")
-	AddComment(4, 2, "this is some content 4")
-	AddComment(4, 1, "this is some content 5")
+	AddComment(2, 1, "Maintaining an exercising regime is the most important")
+	AddComment(1, 1, "I think so too")
+	AddComment(2, 2, "We should have 7 hours of uninterrupted rest")
+	AddComment(4, 2, "This can affect our health adversely")
+	AddComment(4, 1, "I agree")
 
 	AddPopularity(1, 1, 1, false)
 	AddPopularity(2, 1, 1, true)
